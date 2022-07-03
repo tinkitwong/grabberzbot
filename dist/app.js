e993a3b74aab187520f8b4a365272b9687de1abc
tinkitwong <tinkitwong@gmail.com>
2022-07-03 20:13:38 +0800

    test if express is needed

--- dist/app.js
+++ dist/app.js
@@ -5,22 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const express_1 = __importDefault(require("express"));
-const ChadBot_1 = require("./bot/ChadBot");
+const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
 const TOKEN = process.env.BOT_TOKEN;
 const port = process.env.PORT;
-const bot = new ChadBot_1.ChadBot().getBot();
-const app = (0, express_1.default)();
+// const bot: TelegramBot = new ChadBot().getBot()!;
+const bot = new node_telegram_bot_api_1.default(process.env.BOT_TOKEN_DEV, {
+    webHook: {
+        port: port
+    }
+});
+bot.setWebHook(`https://mojojojoz.herokuapp.com:443/bot/${process.env.BOT_TOKEN}`);
+// const app = express();
 // parse the updates to JSON
-app.use(express_1.default.json());
+// app.use(express.json());
 // We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-});
-app.listen(port, () => {
-    console.log(`Express server is listening on ${port}`);
-});
+// app.post(`/bot${TOKEN}`, (req, res) => {
+//   bot.processUpdate(req.body);
+//   res.sendStatus(200);
+// });
+// app.listen(port, () => {
+//   console.log(`Express server is listening on ${port}`);
+// });
 // Just to ping!
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'I am alive!');
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,33 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
+// import express from 'express';
 import { ChadBot } from './bot/ChadBot';
 import TelegramBot from 'node-telegram-bot-api';
 
 const TOKEN = process.env.BOT_TOKEN
 const port = process.env.PORT;
 
-const bot: TelegramBot = new ChadBot().getBot()!;
+// const bot: TelegramBot = new ChadBot().getBot()!;
+const bot = new TelegramBot(process.env.BOT_TOKEN_DEV as string, {
+  webHook: {
+    port: port as unknown as number
+  }
+});
+bot.setWebHook(`https://mojojojoz.herokuapp.com:443/bot/${process.env.BOT_TOKEN}`);
 
-const app = express();
+// const app = express();
 // parse the updates to JSON
-app.use(express.json());
+// app.use(express.json());
 
 // We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+// app.post(`/bot${TOKEN}`, (req, res) => {
+//   bot.processUpdate(req.body);
+//   res.sendStatus(200);
+// });
 
-app.listen(port, () => {
-  console.log(`Express server is listening on ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Express server is listening on ${port}`);
+// });
 
 // Just to ping!
 bot.on('message', msg => {
